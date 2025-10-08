db06b5e582d87a6ff0be73c58c0d7733d417005d
unknown <Samuel>
2025-10-08 16:01:14 +0100

    project init

---
+++ DlpAdmin/DlpAdmin/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DlpAdmin.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
